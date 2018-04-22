f0042efcdb141e776adf94aa8c9daf442e847417
ging7 <g776808@gmail.com>
2018-04-22 21:40:27 +0800

    ArrayAdapter, String[]

--- app/src/main/java/com/greed/ging/note/MainActivity.java
+++ app/src/main/java/com/greed/ging/note/MainActivity.java
@@ -3,13 +3,26 @@ package com.greed.ging.note;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ListView元件設定三筆資料
+        String[] data = {"關於Android",
+                "可愛的小狗狗!",
+                "好聽的音樂！"};
+        int layoutId = android.R.layout.simple_list_item_1;
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, layoutId, data);
+        ListView item_list = (ListView) findViewById(R.id.item_list);
+        item_list.setAdapter(adapter);
     }
 
     @Override
